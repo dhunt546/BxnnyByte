033ed8b93b26029f4f8ea14351487bdab52b39e3
BunnBunnAtGeorgian <145400484+BunnBunnAtGeorgian@users.noreply.github.com>
2023-11-16 12:57:58 -0500

    Ai Moves and navigates

--- Assets/Scripts/Enemy Ai/Enemy Movement scripts/EnemyAI.cs
+++ Assets/Scripts/Enemy Ai/Enemy Movement scripts/EnemyAI.cs
@@ -1,105 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
+
 {
-    [SerializeField]
-    private List<SteeringBehaviour> steeringBehaviours;
+    //Regan Ly
+    //Need to add: weighted desions, fix colliders bc theyre too chunky. Or make new alien type that is smaller as the basic alien
+    public float visionRange = 10f;
+    public float wanderRadius = 5f;
+    public float wanderTimer = 3f;
+    public float groupSeekRange = 15f;
 
-    [SerializeField]
-    private List<Detector> detectors;
+    private Transform player;
+    private Vector3 lastKnownPlayerPosition;
+    private float timer;
+    private NavMeshAgent navMeshAgent;
 
-    [SerializeField]
-    private AIData aiData;
-
-    [SerializeField]
-    private float detectionDelay = 0.05f, aiUpdateDelay = 0.06f, attackDelay = 1f;
-
-    [SerializeField]
-    private float attackDistance = 0.5f;
-
-    //Inputs sent from the Enemy AI to the Enemy controller
-    public UnityEvent OnAttackPressed;
-    public UnityEvent<Vector2> OnMovementInput, OnPointerInput;
-
-    [SerializeField]
-    private Vector2 movementInput;
-
-  [SerializeField]
- private ContextSolver movementDirectionSolver;
-
-    bool following = false;
-
-    private void Start()
+    void Start()
     {
-        //Detecting Player and Obstacles around
-        InvokeRepeating("PerformDetection", 0, detectionDelay);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+
+        // Start with a random destination for wandering
+        SetRandomDestination();
     }
 
-    private void PerformDetection()
+    void Update()
     {
-        foreach (Detector detector in detectors)
+        // Check if player is in vision range
+        if (IsPlayerInVisionRange())
         {
-            detector.Detect(aiData);
-        }
-    }
-
-    private void Update()
-    {
-        //Enemy AI movement based on Target availability
-        if (aiData.currentTarget != null)
-        {
-            //Looking at the Target
-            OnPointerInput?.Invoke(aiData.currentTarget.position);
-            if (following == false)
-            {
-                following = true;
-                StartCoroutine(ChaseAndAttack());
-            }
-        }
-        else if (aiData.GetTargetsCount() > 0)
-        {
-            //Target acquisition logic
-            aiData.currentTarget = aiData.targets[0];
-        }
-        //Moving the Agent
-        OnMovementInput?.Invoke(movementInput);
-    }
-
-    private IEnumerator ChaseAndAttack()
-    {
-        if (aiData.currentTarget == null)
-        {
-            //Stopping Logic
-            Debug.Log("Stopping");
-            movementInput = Vector2.zero;
-            following = false;
-            yield break;
+            lastKnownPlayerPosition = player.position;
+            SeekPlayer();
         }
         else
         {
-            float distance = Vector2.Distance(aiData.currentTarget.position, transform.position);
-
-            if (distance < attackDistance)
-            {
-                //Attack logic
-                movementInput = Vector2.zero;
-                OnAttackPressed?.Invoke();
-                yield return new WaitForSeconds(attackDelay);
-                StartCoroutine(ChaseAndAttack());
-            }
-           else
-           {
-               //Chase logic
-               movementInput = movementDirectionSolver.GetDirectionToMove(steeringBehaviours, aiData);
-               yield return new WaitForSeconds(aiUpdateDelay);
-               StartCoroutine(ChaseAndAttack());
-           }
-
+            // If player is not in vision range, wander or idle
+            WanderOrIdle();
         }
 
+        // Check for other AI to group up
+        GroupUpWithOtherAI();
+    }
+
+    bool IsPlayerInVisionRange()
+    {
+        return Vector3.Distance(transform.position, player.position) <= visionRange;
+    }
+
+    void SeekPlayer()
+    {
+       // Debug.Log("seeking player");
+        // Move towards the last known player position
+        navMeshAgent.SetDestination(lastKnownPlayerPosition);
+    }
+
+    void WanderOrIdle()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            // If timer is greater than wanderTimer, reset timer and set new destination
+            SetRandomDestination();
+            timer = 0;
+        }
+    }
+
+    void SetRandomDestination()
+    {
+        // Generate a random point within the wander radius
+        Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, wanderRadius, 1);
+        Vector3 finalPosition = hit.position;
+
+        // Set the destination to the random point
+        navMeshAgent.SetDestination(finalPosition);
+    }
+
+    void GroupUpWithOtherAI()
+    {
+        //Debug.Log("Grouping up with otherAi");
+        // Find all other Enemy AI in the scene
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (!IsPlayerInVisionRange())
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                // Check if the other AI is within the group seek range and not the same AI
+                if (enemy != gameObject && Vector3.Distance(transform.position, enemy.transform.position) <= groupSeekRange)
+                {
+                    // Move towards the other AI
+                    navMeshAgent.SetDestination(enemy.transform.position);
+                }
+            }
+        }
     }
 }
-
