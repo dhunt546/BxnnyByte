917c2c6edd253ab03594df8b987a5cea2912add5
dhunt546 <200190392@student.georgianc.on.ca>
2023-11-25 10:53:08 -0500

    Update FocusCamera.cs

--- Assets/Scripts/YarnC#/FocusCamera.cs
+++ Assets/Scripts/YarnC#/FocusCamera.cs
@@ -7,34 +7,42 @@ using Yarn.Unity;
 
 public class FocusCamera : MonoBehaviour 
 {
+    //Created by Devin Hunt.
+    //Source: https://docs.yarnspinner.dev/using-yarnspinner-with-unity/creating-commands-functions
+    //Source: https://docs.yarnspinner.dev/unity-tutorial-projects/example-project-3
 
-    // Drag and drop your Dialogue Runner into this variable.
+    // Drag and drop Dialogue Runner into this variable.
     public DialogueRunner dialogueRunner;
+
+    // Drag and drop desired Camera into this varible.
     public GameObject newCamera;
 
     public void Awake()
-    {
+    {   // Initalize DialogueRunner. 
         dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
        
+        // Adds selected Method to end of all Conversations. 
         dialogueRunner.onDialogueComplete.AddListener(CameraDoneLoooking);
     }
-    
-    public void Start()
-    {
-       
-    }
 
+    // Yarnspinner command that calls the dragged and dropped Camera.
     [YarnCommand("focus_camera")]
     public void CameraLookAtTarget()
     {
-
+        // Turns off Main Camera.
         GameObject.Find("Main Camera").GetComponent<Camera>().enabled = false;
+
+        // Turns on dragged and dropped Camera
         newCamera.GetComponent<Camera>().enabled = true;
     }
 
+    // Reverts back to intial Camera being used.
     public void CameraDoneLoooking()
-    {     
-        GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;
+
+    {   // Turns off dragged and dropped Camera
         newCamera.GetComponent<Camera>().enabled = false;
+
+        // Turns on Main Camera.
+        GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;     
     }
 }
