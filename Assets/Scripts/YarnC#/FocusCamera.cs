a2843ba3b857d6d57975ae877965803a13c1f434
dhunt546 <200190392@student.georgianc.on.ca>
2023-11-27 18:46:04 -0500

    fixed

--- Assets/Scripts/YarnC#/FocusCamera.cs
+++ Assets/Scripts/YarnC#/FocusCamera.cs
@@ -5,45 +5,41 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using Yarn.Unity;
 
-public class FocusCamera : MonoBehaviour 
+public class FocusCamera : MonoBehaviour
 {
-    //Created by Devin Hunt.
-    //Source: https://docs.yarnspinner.dev/using-yarnspinner-with-unity/creating-commands-functions
-    //Source: https://docs.yarnspinner.dev/unity-tutorial-projects/example-project-3
-
-    // Drag and drop Dialogue Runner into this variable.
+   
     DialogueRunner dialogueRunner;
-
-    // Drag and drop desired Camera into this varible.
     public GameObject newCamera;
+   
 
-    void Awake()
-    {   // Initalize DialogueRunner. 
-        dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
-       
-        // Adds selected Method to end of all Conversations. 
+    public void Awake()
+    {   
+        dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();     
+    
         dialogueRunner.onDialogueComplete.AddListener(CameraDoneLoooking);
     }
 
-    // Yarnspinner command that calls the dragged and dropped Camera.
-    [YarnCommand("focus_camera")]
-    public void CameraLookAtTarget()
+
+    public void Start()
     {
-
-        // Turns off Main Camera.
-        GameObject.Find("Main Camera").GetComponent<Camera>().enabled = false;
-
-        // Turns on dragged and dropped Camera
-        newCamera.GetComponent<Camera>().enabled = true;
+        newCamera.GetComponent<Camera>().enabled = false;
+    }
+    
+  
+    [YarnCommand("focusCamera")]
+    public void focusCamera()
+    {       
+        newCamera.GetComponent<Camera>().enabled = true;                
     }
 
-    // Reverts back to intial Camera being used.
+    [YarnCommand("doneFocus")]
+    public void doneFocus()
+    {         
+            newCamera.GetComponent<Camera>().enabled = false;    
+    }
+   
     public void CameraDoneLoooking()
-
-    {   // Turns off dragged and dropped Camera
-        newCamera.GetComponent<Camera>().enabled = false;
-
-        // Turns on Main Camera.
-        GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;     
+    {
+        newCamera.GetComponent<Camera>().enabled = false;           
     }
 }
