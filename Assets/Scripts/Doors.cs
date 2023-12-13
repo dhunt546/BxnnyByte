b468da6c78035c90a687c2f77474866300304503
dhunt546 <145506495+dhunt546@users.noreply.github.com>
2023-12-13 16:21:57 -0500

    fixing door script

--- Assets/Scripts/Doors.cs
+++ Assets/Scripts/Doors.cs
@@ -5,96 +5,39 @@ using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
-    public Sprite openDoorSprite;
-    public Sprite closedDoorSprite;
-    public float doorOpenTime = 1f;
-    public bool is_door_open = false;
+    public GameObject doorPrefab;
 
     private Animator doorAnimator;
-    private SpriteRenderer doorSpriteRenderer;
+ 
+    bool doorStatus = false;
 
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        doorAnimator = GetComponent<Animator>();
-    }
     void Start()
     {
-
-        doorSpriteRenderer = GetComponent<SpriteRenderer>();
-
-        SetDoorState(true);
-
+        doorAnimator = GetComponent<Animator>();        
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (is_door_open)
-        {
-            doorSpriteRenderer.sprite = openDoorSprite;
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {                        
+            if(doorStatus == false)
+            {
+                doorAnimator.SetBool("doorStatus", true);
+                doorStatus = true;
+                doorPrefab.GetComponent<EdgeCollider2D>().enabled = false;
+            }
         }
-        else if (!is_door_open)
-        {
-            doorSpriteRenderer.sprite = closedDoorSprite;
-        }
-
-      //  if (Input.GetKeyDown(KeyCode.E))
-      //  {
-      //      if (Input.GetKeyDown(KeyCode.E) && is_door_open == false)
-      //      {
-      //          // Play the door opening animation
-      //          doorAnimator.SetTrigger("OpenDoor");
-      //
-      //          // Wait for the door to open before changing the sprite
-      //          Invoke("OpenDoor", doorOpenTime);
-      //      }
-      //      else if (Input.GetKeyDown(KeyCode.E) && is_door_open == true)
-      //      {
-      //          // Play the door closing animation
-      //          doorAnimator.SetTrigger("CloseDoor");
-      //
-      //          // Wait for the door to close before changing the sprite
-      //          Invoke("CloseDoor", doorOpenTime);
-      //      }
-      //
-      //  }
     }
-
-    void OpenDoor()
+    void OnTriggerExit2D(Collider2D other)
     {
-        SetDoorState(true);
-    }
-
-    void CloseDoor()
-    {
-        SetDoorState(false);
-    }
-
-    void SetDoorState(bool doorOpen)
-    {
-        is_door_open=doorOpen;
-        // Set the door sprite based on the state
-        doorSpriteRenderer.sprite = doorOpen ? openDoorSprite : closedDoorSprite;
-    }
-    // Animation Event Functions
-    void OnDoorOpened()
-    {
-        OpenDoor();
-    }
-
-    void OnDoorClosed()
-    {
-        CloseDoor();
-    }
-
-    // You may want to use OnTriggerEnter2D or OnTriggerStay2D based on your needs
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            // Display a prompt or handle player interaction as needed
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {                     
+            if (doorStatus == true)
+            {
+                doorAnimator.SetBool("doorStatus", false);
+                doorStatus = false;
+                doorPrefab.GetComponent<EdgeCollider2D>().enabled = true;
+            }
         }
     }
 }
