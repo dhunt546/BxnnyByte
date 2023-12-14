1e039282fb024c0441ba88abdcb77270099df483
dhunt546 <145506495+dhunt546@users.noreply.github.com>
2023-12-13 20:24:07 -0500

    working cutscene dialogue and fix hud slider

---
+++ Assets/Scripts/YarnC#/WhenDialogueIsRunning.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class WhenDialogueIsRunning : MonoBehaviour
+{
+    DialogueRunner dialogueRunner;
+
+    GameObject player;
+    GameObject enemy;
+
+    void Start()
+    {
+        dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+        player = GameObject.FindWithTag("Player");
+        enemy = GameObject.FindWithTag("Enemy");
+    }
+
+  
+    void Update()
+    {
+        Conversing();
+    }
+
+    void Conversing()
+    {
+        player = GameObject.FindWithTag("Player");
+        enemy = GameObject.FindWithTag("Enemy");
+
+        if (dialogueRunner.IsDialogueRunning == true)
+        {
+            if (player != null)
+            {
+                player.GetComponent<PlayerMovement>().enabled = false;
+            }
+            if (enemy != null)
+            {
+                enemy.GetComponent<EnemyAI>().enabled = false;
+            }
+        }
+        else if (dialogueRunner.IsDialogueRunning == false)
+        {
+            if (player != null)
+            {
+                player.GetComponent<PlayerMovement>().enabled = true;
+            }
+            if (enemy != null)
+            {
+                enemy.GetComponent<EnemyAI>().enabled = true;
+            }
+        }
+    }
+
+}
