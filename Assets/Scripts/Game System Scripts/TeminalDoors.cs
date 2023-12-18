6661d9d7e6aab1aaf4d4c4790c04a89bc2dc0b4b
dhunt546 <145506495+dhunt546@users.noreply.github.com>
2023-12-18 15:48:46 -0500

    add door interaction with terminal

---
+++ Assets/Scripts/Game System Scripts/TeminalDoors.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeminalDoors : MonoBehaviour
+{
+    public Sprite lockedTerminal;
+    public Sprite unlockedTerminal;
+
+    public GameObject correspondingDoor;
+
+    private Animator doorAnimator;
+
+    bool isDoor;
+    bool isInteractable;
+
+    float doorTimer = 1.0f;
+    float currentTime;
+
+    void Start()
+    {
+        currentTime = doorTimer;
+
+        doorAnimator = correspondingDoor.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+     
+        if (Input.GetKeyDown(KeyCode.E) && isInteractable == true)
+        {
+            if (isDoor == true && currentTime >= doorTimer) 
+            {
+                TerminalUnlocked();               
+            }
+            if (isDoor == false && currentTime >= doorTimer)
+            {
+                TerminalLocked();              
+            }
+        }            
+    }
+
+    void TerminalUnlocked()
+    {
+        isDoor = false;
+
+        currentTime = 0.0f;
+
+        doorAnimator.SetBool("doorStatus", true);
+        
+        gameObject.GetComponent<SpriteRenderer>().sprite = unlockedTerminal;
+        
+        correspondingDoor.GetComponent<CapsuleCollider2D>().enabled = false;
+    }
+
+    void TerminalLocked()
+    {   
+        isDoor = true;
+
+        currentTime = 0.0f;
+
+        doorAnimator.SetBool("doorStatus", false);
+
+        gameObject.GetComponent<SpriteRenderer>().sprite = lockedTerminal;
+
+        correspondingDoor.GetComponent<CapsuleCollider2D>().enabled = true;      
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {        
+            isInteractable = true; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {         
+            isInteractable = false;
+        }
+    }
+}
