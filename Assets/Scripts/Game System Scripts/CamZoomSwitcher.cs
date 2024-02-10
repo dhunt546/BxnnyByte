54ff4ab1bc13d2d7ccdb529361dc4db109a073d5
dhunt546 <145506495+dhunt546@users.noreply.github.com>
2024-02-10 09:22:19 -0500

    finish cam zoom script

--- Assets/Scripts/Game System Scripts/CamZoomSwitcher.cs
+++ Assets/Scripts/Game System Scripts/CamZoomSwitcher.cs
@@ -1,29 +1,48 @@
 using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CamZoomSwitcher : MonoBehaviour
 {
-    public float camSizeValue;
-
     [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    public float camDefaultSize = 8.0f;
+    public float camNewSizeValue;
+    public float zoomSpeed = 4.0f;
+
+    bool isZoomingIn = true;
+    bool isZoomingActive;
 
     void CamZoomHandler()
     {
-        float zoomSpeed = 0.5f;
-        
-        cinemachineVirtualCamera.m_Lens.OrthographicSize = 
-            Mathf.Lerp(cinemachineVirtualCamera.m_Lens.OrthographicSize, camSizeValue, zoomSpeed + Time.deltaTime);
-        
+        if (!isZoomingActive) return;
+
+        float targetSize = isZoomingIn ? camNewSizeValue : camDefaultSize;
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = Mathf.MoveTowards(
+            cinemachineVirtualCamera.m_Lens.OrthographicSize, targetSize, zoomSpeed * Time.deltaTime);
+
+        if (Mathf.Abs(cinemachineVirtualCamera.m_Lens.OrthographicSize - targetSize) < 0.01f)
+        {
+            cinemachineVirtualCamera.m_Lens.OrthographicSize = targetSize;
+            isZoomingIn = !isZoomingIn;
+            isZoomingActive = false;
+        }
+
+        //Debug.Log("Cam current size = " + cinemachineVirtualCamera.m_Lens.OrthographicSize);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            Debug.Log(other.name);
-            CamZoomHandler();
+            isZoomingActive = true;
         }
+    }
+
+    private void Update()
+    {
+        CamZoomHandler();
     }
 }
